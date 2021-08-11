316a195e81cc6e6bf19ee16fb294bb16323d9e88
Abel Zambrano <azambrano1417@gmail.com>
2021-08-10 18:08:23 -0700

    add global style

--- components/Page.tsx
+++ components/Page.tsx
@@ -1,18 +1,65 @@
+import styled, { createGlobalStyle } from "styled-components";
 import Header from "./header/Header";
 import Footer from "./footer/Footer";
+
+const GlobalStyles = createGlobalStyle`
+:root {
+  --black-dark: rgb(22, 22, 22);
+  --black-light: rgb(29, 29, 29);
+  --white: #fff;
+  --off-white: #f2f2f2;
+  --primary-blue: #0262bc;
+  --primary-orange:  #ffa64d;
+  --gray-blue: rgb(90, 99, 125);
+}
+html {
+    box-sizing: border-box;
+    font-size: 62.5%;
+    letter-spacing: 0.1rem;
+    scroll-behavior: smooth;
+}
+body {
+  padding: 0;
+  margin: 0;
+  height: 100%;
+  font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+  Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+}
+a {
+  color: inherit;
+  text-decoration: none;
+}
+* {
+  margin: 0;
+  padding: 0;
+}
+*::before,
+*::after {
+  box-sizing: inherit;
+}
+`;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+`;
+
+const Content = styled.div`
+  flex: 1;
+`;
 
 type Props = {
   children?: any;
 };
 
-const Page = ({ children }: Props) => {
+export default function Page({ children }: Props) {
   return (
-    <>
-      <Header className="header" title="Papertoshi" />
-      <div>{children}</div>
+    <Container>
+      <GlobalStyles />
+      <Header title="Papertoshi" />
+      <Content>{children}</Content>
       <Footer />
-    </>
+    </Container>
   );
-};
-
-export default Page;
+}
