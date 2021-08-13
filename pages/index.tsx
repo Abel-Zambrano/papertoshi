73c3eb07b6cdd6811092ccc43a38e24f273ce1f4
Abel Zambrano <azambrano1417@gmail.com>
2021-08-12 20:46:06 -0700

    add crypto api

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import Cryptos from "../components/Cryptos";
 import Page from "../components/Page";
 
 const MyPage = styled.div`
@@ -12,6 +13,7 @@ export default function Home() {
       <Page>
         <MyPage>
           <h1 style={{ color: "white" }}>Page Content</h1>
+          <Cryptos />
         </MyPage>
       </Page>
     </>
