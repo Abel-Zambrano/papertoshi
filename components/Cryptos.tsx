b910254facf48f5f16f210da8bd4ed2098d11dee
Abel Zambrano <azambrano1417@gmail.com>
2021-08-28 18:08:38 -0700

    changed api to call 14 top coins

--- components/Cryptos.tsx
+++ components/Cryptos.tsx
@@ -17,7 +17,7 @@ export default function Cryptos() {
   const [coins, setCoins] = useState([]);
 
   const geckoAPI =
-    "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=20&page=1&sparkline=false";
+    "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=bitcoin%2Cethereum%2Ccardano%2Cbinancecoin%2Cripple%2Cdogecoin%2Cpolkadot%2Csolana%2Cchainlink%2Clitecoin%2Cmatic-network%2Ctezos%2Camp-token%2Cstellar&order=market_cap_desc&per_page=100&page=1&sparkline=false";
 
   const getData = async () => {
     try {
