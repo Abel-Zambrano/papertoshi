be5cb2d5d1518f26172a40ea68ff58717f1f953f
Abel Zambrano <azambrano1417@gmail.com>
2021-10-09 09:37:43 -0700

    test

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,11 @@
-import styled from "styled-components";
+import Cryptos from "../components/Cryptos";
 import Page from "../components/Page";
-
-const MyPage = styled.div`
-  display: flex;
-  justify-content: center;
-  background-color: var(--white);
-  height: 100%;
-`;
 
 export default function Home() {
   return (
     <>
       <Page>
-        <MyPage></MyPage>
+        <Cryptos />
       </Page>
     </>
   );
--- pages/trade.tsx
+++ pages/trade.tsx
@@ -2,9 +2,5 @@ import Page from "../components/Page";
 import Cryptos from "../components/Cryptos";
 
 export default function Trade() {
-  return (
-    <Page>
-      <Cryptos />
-    </Page>
-  );
+  return <Page>{/* <Cryptos /> */}</Page>;
 }
