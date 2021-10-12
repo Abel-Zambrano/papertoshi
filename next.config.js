8a0d8f1319e3572cd295be6506d7fd35a6c48512
Abel Zambrano <azambrano1417@gmail.com>
2021-10-11 22:22:52 -0700

    update

--- next.config.js
+++ next.config.js
@@ -1,9 +1,5 @@
 module.exports = {
   images: {
-    domains: [
-      `assets.coingecko.com`,
-      `api.coingecko.com`,
-      `images.unsplash.com`,
-    ],
+    domains: [`images.unsplash.com`, `assets.coingecko.com`],
   },
 };
