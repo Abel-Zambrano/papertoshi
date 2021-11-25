54ffd31b404a878867fe6e58d80e3254cb793ee8
Abel Zambrano <azambrano1417@gmail.com>
2021-11-25 13:57:34 -0800

    changed whcih page features the search bar

--- components/Heading.tsx
+++ components/Heading.tsx
@@ -21,7 +21,7 @@ export default function Heading() {
   return (
     <MyHeading>
       <PageTitle title={pathName} />
-      <SearchBar />
+      {pathName === "market" ? <SearchBar /> : null}
       <UserInfo />
     </MyHeading>
   );
