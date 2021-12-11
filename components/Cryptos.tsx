49f66dcccaeb77d8a0dfe455c9cf3f133f00bc27
Abel Zambrano <azambrano1417@gmail.com>
2021-12-10 20:56:09 -0800

    update tokens for trade

--- components/Cryptos.tsx
+++ components/Cryptos.tsx
@@ -9,7 +9,7 @@ export default function Cryptos() {
   const dispatch = useDispatch();
 
   const geckoTradeAPI =
-    "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=bitcoin%2Cethereum%2Ccardano%2Cbinancecoin%2Cripple%2Cdogecoin%2Cpolkadot%2Csolana%2Cchainlink%2Clitecoin%2Cmatic-network%2Ctezos%2Camp-token%2Cstellar&order=market_cap_desc&per_page=100&page=1&sparkline=false";
+    "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=bitcoin%2Cethereum%2Ccardano%2Cbinancecoin%2Cdogecoin%2Cpolkadot%2Csolana%2Cterra-luna%2Cmatic-network%2Ctezos%2Cavalanche-2%2Cshiba-inu&order=market_cap_desc&per_page=100&page=1&sparkline=false";
 
   const getData = async () => {
     try {
