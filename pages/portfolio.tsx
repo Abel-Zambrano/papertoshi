fa30a3fddd00febf8370ab7e6bddedb1bed02010
Abel Zambrano <azambrano1417@gmail.com>
2021-12-22 18:37:54 -0800

    update portfolio page

--- pages/portfolio.tsx
+++ pages/portfolio.tsx
@@ -1,5 +1,31 @@
 import Page from "../components/Page";
+import styled from "styled-components";
+import Image from "next/image";
+
+const ComingSoon = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const Title = styled.h1`
+  padding: 20px;
+`;
 
 export default function Portfolio() {
-  return <Page></Page>;
+  return (
+    <Page>
+      <ComingSoon>
+        <Title>Portfolio Coming Soon</Title>
+        <Image
+          src="/portfolio.jpg"
+          width="600"
+          height="500"
+          blurDataURL="/portfolio.jpg"
+          placeholder="blur"
+        />
+      </ComingSoon>
+    </Page>
+  );
 }
