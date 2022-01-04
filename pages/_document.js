997bdcdde6dd6980c36d996cdc6c620123e1c749
Abel Zambrano <azambrano1417@gmail.com>
2022-01-03 20:27:52 -0800

    removed meta

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,4 @@
-import Document, { Html, Head, Main, NextScript, Meta } from "next/document";
+import Document, { Html, Head, Main, NextScript } from "next/document";
 export default class MyDocument extends Document {
   static async getInitialProps(ctx) {
     const initialProps = await Document.getInitialProps(ctx);
@@ -9,10 +9,6 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <Meta
-            name="viewport"
-            content="width=device-width, initial-scale=1, maximum-scale=1"
-          />
           <link
             href="https://fonts.googleapis.com/css2?family=Mulish:wght@400;700&display=swap"
             rel="stylesheet"
