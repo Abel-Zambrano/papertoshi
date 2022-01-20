61b77778e25910a22f38f55512d17c29a348fb9c
Abel Zambrano <azambrano1417@gmail.com>
2022-01-19 20:29:25 -0800

    update meta

--- pages/_document.js
+++ pages/_document.js
@@ -9,6 +9,10 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, maximum-scale=1"
+          />
           <link
             href="https://fonts.googleapis.com/css2?family=Mulish:wght@400;700&display=swap"
             rel="stylesheet"
